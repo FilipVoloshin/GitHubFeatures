4eee2a10d65b3f89e5b77e7559e3249f2c20310e
devreactor_f <lajdakua@gmail.com>
2017-11-17 22:58:34 +0200

    created ajax method for case 1

---
+++ GitHubFeatures/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿
+$('#githubinfo_submit').click(function () {
+    var token = $('input[name="__RequestVerificationToken"]', $('#githubinfo_form')).val();
+    var dataToSend = {
+        userName: $('#users_name').val(),
+        repositoryName: $('#repo_name').val(),
+        requestType: $("#request_type").val()
+    };
+
+    var dataWithAntiforgeryToken = $.extend(dataToSend, { '__RequestVerificationToken': token });
+
+    $.ajax({
+        url: "/Home/Check",
+        type: "POST",
+        data: dataWithAntiforgeryToken
+        //success: function (data) {
+        //},
+        //error: function () {
+        //}
+    });
+});
