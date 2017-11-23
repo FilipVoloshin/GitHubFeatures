87bca114bd1e272e65b2d6a098676cac4cb0ac30
devreactor_f <lajdakua@gmail.com>
2017-11-23 16:00:12 +0200

    check content file

--- GitHubFeatures/wwwroot/js/site.js
+++ GitHubFeatures/wwwroot/js/site.js
@@ -1,5 +1,4 @@
-﻿
-$('#githubinfo_submit').click(function () {
+﻿$('#githubinfo_submit').click(function () {
     var requestType = $("#request_type").val();
     var token = $('input[name="__RequestVerificationToken"]', $('#githubinfo_form')).val();
     var dataToSend = {
@@ -23,14 +22,49 @@ $('#githubinfo_submit').click(function () {
         case "3":
             url = "/Home/CheckCommits";
             break;
+        case "4":
+            url = "https://raw.githubusercontent.com/" + dataToSend.userName + "/" + dataToSend.repositoryName + "/" + $("#path_value").val();
     }
-    $.ajax({
-        url: url,
-        type: "POST",
-        data: dataWithAntiforgeryToken,
-        success: function (data) {
-            $("#result_window_pl").html(data);
-            $("#result_window").modal('show');
+
+    if (requestType !== "4") {
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: dataWithAntiforgeryToken,
+            success: function (data) {
+                $("#result_window_pl").html(data);
+                $("#result_window").modal('show');
+            }
+        });
+    }
+    else {
+        if (dataToSend.userName && dataToSend.repositoryName && $("#path_value").val()) {
+            $.get(url, function (data) {
+                var win = window.open(url, '_blank');
+                if (win) {
+                    win.focus();
+                } else {
+                    alert('Please allow popups for this website');
+                }
+            }).fail(function () {
+                alert("The dirrection " + url + " does not exist");
+            });
         }
-    });
+        else {
+            alert("Fill in all fields");
+        }
+    }
+});
+
+function showHideAdditionalInfo(requestType) {
+    if (requestType === "4") {
+        $("#path_div").show();
+    }
+    else {
+        $("#path_div").hide();
+    }
+}
+
+$("#request_type").change(function () {
+    showHideAdditionalInfo($("#request_type").val());
 });
